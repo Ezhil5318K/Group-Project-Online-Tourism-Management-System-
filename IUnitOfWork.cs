c46d98f83a69dc28fc09c3a9a9ab3c092efef8c9
KetakiUbale <70467081+KetakiUbale@users.noreply.github.com>
2020-10-19 11:50:52 +0530

    Repository and unitofwork created

---
+++ IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tourism.Model;
+
+namespace Tourism.Persistence
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        Repository<User> UserRepository { get; }
+        int Save();
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tourism.Model;
+
+namespace Tourism.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TourismContext _context;
+        public Repository<User> UserRepository { get; }
+        public UnitOfWork(TourismContext context)
+        {
+            this._context = context;
+            this.UserRepository = new Repository<User>(this._context);
+        }
+        public int Save()
+        {
+            return this._context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            this._context.Dispose();
+        }
+        
+
+    }
+        
+        
+}
